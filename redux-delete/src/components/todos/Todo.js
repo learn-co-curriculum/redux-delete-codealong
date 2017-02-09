f23de50b0d3640192ff33f75e49feb432798c5e5
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 14:12:35 -0500

    convert Todo component to use handleOnClick()

--- redux-delete/src/components/todos/Todo.js
+++ redux-delete/src/components/todos/Todo.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Todo extends Component {
-  handleClick(){
-    
-    this.props.store.dispatch({type: 'DELETE_TODO', payload: this.props.id})
+
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_TODO',
+      id: this.props.id,
+    });
   }
-  render(){
+
+  render() {
     return (
       <li>
         {this.props.text}
-        <button onClick={this.handleClick.bind(this)}>Delete </button>
+        <button onClick={() => this.handleOnClick()}>X</button>
       </li>
-    )
+    );
   }
-}
+};
 
 export default Todo
